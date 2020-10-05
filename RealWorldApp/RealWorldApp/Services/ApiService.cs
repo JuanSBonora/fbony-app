c3bb85572d84ef9fb0eaebed5af7790637f7dc77
JuanSBonora <juanfcosantiagob1002@outlook.com>
2020-10-05 01:08:04 -0500

    ApiService - OrderDetails

--- RealWorldApp/RealWorldApp/Services/ApiService.cs
+++ RealWorldApp/RealWorldApp/Services/ApiService.cs
@@ -123,5 +123,19 @@ namespace RealWorldApp.Services
             var jsonResult = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<OrderResponse>(jsonResult);
         }
+        public static async Task<List<OrderByUser>> GetOrdersByUser(int userId)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("accessToken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "api/Orders/OrdersByUser/" + userId);
+            return JsonConvert.DeserializeObject<List<OrderByUser>>(response);
+        }
+        public static async Task<List<Order>> GetOrderDetails(int orderId)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("accessToken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "api/Orders/OrderDetails/" + orderId);
+            return JsonConvert.DeserializeObject<List<Order>>(response);
+        }
     }
 }
