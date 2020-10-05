81842f26936bc6c01a90360c2384be70d59140d3
JuanSBonora <juanfcosantiagob1002@outlook.com>
2020-10-04 21:27:12 -0500

    ApiService - GetProductById

---
+++ RealWorldApp/RealWorldApp/Services/ApiService.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using RealWorldApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace RealWorldApp.Services
+{
+    public class ApiService
+    {
+        public async Task<bool> RegisterUser(string name, string email, string password)
+        {
+            var register = new Register()
+            {
+                Name = name,
+                Email = email,
+                Password = password
+            };
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(register);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync(AppSettings.ApiUrl + "api/Accounts/Register", content);
+            if (!response.IsSuccessStatusCode) return false;
+            return true;
+        }
+        public static async Task<bool> Login(string email, string password)
+        {
+            var login = new Login()
+            {
+                Email = email,
+                Password = password
+            };
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(login);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync(AppSettings.ApiUrl + "api/Accounts/Login", content);
+            if (!response.IsSuccessStatusCode) return false;
+            var jsonResult = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Token>(jsonResult);
+            Preferences.Set("AccessToken", result.access_token);
+            Preferences.Set("userId", result.user_Id);
+            Preferences.Set("userName", result.user_name);
+            return true;
+        }
+        public static async Task<List<Category>> GetCategories()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("AccessToken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "api/Categories");
+            return JsonConvert.DeserializeObject<List<Category>>(response);
+        }
+        public static async Task<Product> GetProductById(int productId)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("AccessToken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "api/Products/" + productId);
+            return JsonConvert.DeserializeObject<Product>(response);
+        }
+    }
+}
